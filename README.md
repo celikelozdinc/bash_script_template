a30193b4c794112b56a6b1abb5ab8e1f6f2987a2
celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>
2022-01-12 12:53:37 +0300

    shellcheck, uname commands, send desktop notification
    Signed-off-by: celikelozdinc <niyazi.celikel@ulakhaberlesme.com.tr>

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 [Reporting docker resource usage](https://medium.com/geekculture/checklist-for-a-clean-efficient-developer-workstation-4a000c83073e)
 
 [trash-cli](https://github.com/andreafrancia/trash-cli)
+
+[Sending desktop notifications](https://opensource.com/article/22/1/linux-desktop-notifications)
+
+[Disabling ShellCheck controls](https://medium.com/mkdir-awesome/the-ultimate-guide-to-modularizing-bash-script-code-f4a4d53000c2)
--- cleanup.sh
+++ cleanup.sh
@@ -7,10 +7,12 @@ current_dir=$(dirname "$(readlink --canonicalize-existing "${0}" 2> /dev/null)")
 readonly conf_file="${current_dir}/conf.env"
 readonly utility_file="${current_dir}/utility.sh"
 readonly error_no_such_configuration_file_found=17
-readonly error_no_such_container_found=41
+#readonly error_no_such_container_found=41
 
-
-source "${utility_file}"
+# shellcheck disable=SC1090
+{
+    source "${utility_file}"
+}
 
 # Read configuration file
 if [ ! -f "${conf_file}" ] ; then
@@ -19,7 +21,15 @@ if [ ! -f "${conf_file}" ] ; then
         "${error_no_such_configuration_file_found}"
 fi
 
-source "${conf_file}"
+# shellcheck disable=SC1090
+{
+    source "${conf_file}"
+}
+
+export NODE=$(uname -n)
+if [[ $(uname) == "Linux"  ]]; then
+	echo "Performing housekeeping activites for \"Linux"\" on node : "${NODE}"
+fi
 
 disk_usage_report "BEFORE"
 
@@ -36,6 +46,9 @@ docker builder prune --all --force
 
 # Clean exited docker containers if any
 if [ "$(docker ps -aq)" ]; then
+    echo "Sending desktop notification..."
+    #notify-send -i dialog-error 'Will prune some containers!' -t 4000
+    xmessage "Just to inform you! Will prune some containers!"
     clean_exited_containers
 fi
 
